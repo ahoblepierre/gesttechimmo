968256aab3feb12115d4d13c7d0dbee3035d716a
osx <tinitzdev@192.168.1.11>
2023-10-06 11:27:31 +0000

    Ajout de template et certains models

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    //
+
+
+    /**
+     * View to login user admin
+     *
+     * @return View
+     */
+    function toLogin() : View {
+        return view("pages.login");
+    }
+
+
+    /**
+     * Get request to logue user admin
+     *
+     * @param Request $request
+     * @return View
+     */
+    function login(Request $request) : View {
+        var_export($request);
+        return view("admin.pages.dashboard");
+    }
 }
